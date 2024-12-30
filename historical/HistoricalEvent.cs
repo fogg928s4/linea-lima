c24a150058be7bbe3cb3a052298225e2ec73abe4
J Melg <rjmzelaya@gmail.com>
2024-12-29 23:24:58 -0600

    First commit

---
+++ historical/HistoricalEvent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+///<summary>
+///This class includes the elements of a dated event (battle, death, victory)
+///It does not include events that happen over a period of time (like a battle or a reaaally slow death
+///It includes the necessary elemtns from the get go
+///</summary>
+
+namespace Linea_Lima.historical{
+    public class HistoricalEvent {
+
+    }
+}
---
+++ historical/Persona.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// This class is meant to hold the information of people or characters or institutions
+/// that participated on a given event or series of events
+/// </summary>
+namespace Linea_Lima.historical {
+    internal class Persona {
+        private string name;
+        private int age;
+        private string title;
+        private string origin; //country or region of origin
+        
+
+        //empty constructor
+        public Persona() {
+            name = string.Empty;
+            age = 0;
+            title = string.Empty;
+            origin = string.Empty;
+        }
+        //filled constructors
+        public Persona(string name, int age, string title, string origin) {
+            this.name = name;
+            this.age = age;
+            this.title = title;
+            this.origin = origin;
+        }
+
+        //public properties 
+        public string Name { get { return name; } set { name = value; } }
+        public int Age { get { return age; } set { age = value; } }
+        public string Title { get { return title; } set { title = value; } }
+        public string Origin { get { return origin; } set { origin = value; } }
+
+        //ill do methods as I go
+
+    }
+}
