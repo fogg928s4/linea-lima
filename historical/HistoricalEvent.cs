4e8d560b565b99161e974d34ea32b21cb5e10f19
Jose M <rjmzelaya@gmail.com>
2024-12-31 13:13:47 -0600

    Added historical period, place and date class

--- historical/HistoricalEvent.cs
+++ historical/HistoricalEvent.cs
@@ -7,11 +7,13 @@ using System.Threading.Tasks;
 ///<summary>
 ///This class includes the elements of a dated event (battle, death, victory)
 ///It does not include events that happen over a period of time (like a battle or a reaaally slow death
-///It includes the necessary elemtns from the get go
+///It includes the necessary elements from the get go
 ///</summary>
 
 namespace Linea_Lima.historical{
     public class HistoricalEvent {
-
+        private List<Persona> involved;
+        //struct for the Date
+        
     }
 }
