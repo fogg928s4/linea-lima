3e526423ac3e9bdf5ac4a7801f2b72000970f49c
J Melg <rjmzelaya@gmail.com>
2025-01-04 21:14:41 -0600

    For family tree

---
+++ family/figure.cs
@@ -0,0 +1,20 @@
+﻿using historian.historical;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+///<summary>
+///This class includes information about a historical figure
+///this is meant to be used on the family tree thingy
+///really fun
+///It's inherited from hsitorian.historical.Persona
+///</summary>
+
+
+namespace historian.family {
+    internal class figure : Persona {
+
+    }
+}
